71085e4d9e9306ba6b75fc890de0c8ad1ea32c0a
Johny Walker~ <johnywalker696@gmail.com>
2022-04-26 14:02:09 -0300

    updata

---
+++ Php/cursophp/aaulaas/20-04.php
@@ -0,0 +1,32 @@
+<?php
+
+/* 1 - Escreva um algoritmo que leia um número e imprima a raiz quadrada
+do número caso ele seja positivo ou igual a zero e o quadrado do número caso ele seja
+negativo. */
+
+/*
+    $numero = readline("Informe um numero: ");
+
+    if ($numero >= 0){
+        echo "A raiz do numero é : $numero "; 
+    }
+
+    if ($numero <= -1){
+        echo "O quadrado do numero é : " . ($numero * $numero);
+    }
+*/
+
+//3 - Faça um algoritmo que conte até o número que o usuário informar
+
+
+/*
+    $contador = 10;
+    $valor = readline("Quer contar até quanto: ");
+    while ($contador >= $valor)
+    echo "$contador";
+    $contador = ($contador - 1);
+
+*/
+
+
+?>
---
+++ Php/cursophp/aaulaas/25-04.php
@@ -0,0 +1,68 @@
+<?php
+ 
+ /*1) Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.  */
+
+ /*
+    $a = readline("Escreva o valor de A: ");
+    $b = readline("Escreva o valor de B: ");
+    $c = readline("Escreva o valor de C: ");
+
+    $s = $a + $b ;
+
+    //echo "$s";
+
+    if ($s < $c){
+        echo "valor de C = $c e A + B = $s , sendo assim A + B é menor que C.";
+    }elseif ($s > $c){
+        echo "valor de C = $c e A + B = $s , sendo assim A + B é maior que C.";
+    }elseif($s == $c){
+        echo "valor de C = $c e A + B = $s , sendo assim A + B é igual a C.";
+    }
+*/ 
+
+
+/*3) Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.  */
+
+
+/*    
+    $n = readline("Escreva um numero: ");
+
+    if ($n % 2 == 0){
+       echo "O numero $n é par !";
+    }else{
+       echo "O numero $n é impar !";
+    }   
+*/
+
+
+/*4) Faça um algoritmo que leia dois valores inteiros A e B se os valores forem iguais deverá se somar os dois, caso contrário multiplique A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado para uma variável C e mostrar seu conteúdo na tela.  */
+
+/*
+    $a = readline("Informe o primeiro numero: ");
+    $b = readline("Informe o segundo numero: ");
+    $c = readline("Informe o terceiro numero: ");
+
+    if($b == $a){
+       
+        echo "Os valores são iguais, portanto o resultado da somatória é igual a: $c";
+    }elseif($c == ($a * $b)){
+        echo "Os valores são diferentes, portanto o resultado da mulplicação é igual a:, $c";
+    }
+*/
+
+
+/*6) ) Faça um algoritmo que leia uma variável e some 5 caso seja par ou some 8 caso seja ímpar, imprimir o resultado desta operação. */
+
+/*
+    $n = readline("Escreva um numero: ");
+
+    if ($n % 2 == 0){
+        $res = $n + 5;
+        echo "Resultado = $n , + 5 = $res";
+    }else{
+        $res = $n + 8;
+        echo "Resultado = $n , + 8 = $res";
+    }
+*/
+
+?>
