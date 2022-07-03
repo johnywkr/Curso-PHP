2e4d6aa517495dacde187a8262a921003320f593
johne <johnywalker696@gmail.com>
2022-07-03 15:47:51 -0300

    revisao

---
+++ Php/cursophp/aula06_POO 1 - classes e objetos/index.php
@@ -0,0 +1,16 @@
+<?php 
+require_once 'Conta.php';
+
+$c1 = new Conta();
+$c1->titular = "Johne";
+$c1->tipo = "Conta Corrente";
+$c1->limite = 1000;
+$c1->status = false;
+$c1->saldo = 7000;
+
+
+$c1->abrirConta();
+$c1->depositar(500);
+$c1->sacar(8000);
+
+print_r($c1);
