81ed627bfc95058b89dd39b0e1d0bdc491eddc1a
johne <johnywalker696@gmail.com>
2023-09-03 16:46:07 -0300

    funçoes

---
+++ Php/cursophp/aula05_Funcoes/exp01.php
@@ -0,0 +1,22 @@
+Aprendendo Funçoes do PHP
+<?
+
+$produto = "Cafe";
+$preco = 4.5;
+$mercado = "Tatico";
+
+echo "O produto é $produto e o preço é " . number_format($preco,2 ). "\n";
+
+//Tambem pode ser escrito de outra forma com printf, LEIA ABAIXO:
+
+printf("O %s custa %.2f no Supermercado %s",$produto,$preco,$mercado);
+// $s busca o valor string e $f busca o valor float.
+
+/*
+%d = Recupera um dado inteiro positivo e negativo.
+%u = Recupera um dado inteiro positivo.
+%f = Recupera um dado float.
+%s = Recupera uma string.
+*/
+
+//aula parou min 12:45
